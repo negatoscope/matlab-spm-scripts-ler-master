ec66e939425ac7798a49a307aa88458919b6ad6f
Luis Eudave <38909206+negatoscope@users.noreply.github.com>
2021-03-17 13:40:42 +0100

    Update spm_specify2nd_ler_job.m

--- spm_specify2nd_ler_job.m
+++ spm_specify2nd_ler_job.m
@@ -1,15 +1,18 @@
-function spm_specify2ndlevel_ler(stats_dir, input)
+function spm_specify2nd_ler_job(stats_dir, input)
 
 spm('defaults','fmri');
 spm_jobman('initcfg');
 second_level = struct;
-func4D_spm = spm_vol(func4D_fn);
-func4D_size = size(func4D_spm);
-Nt = func4D_size(1);
+spm_size = size(input);
+n_sub = spm_size(2);
+fcon={};
+for i = 1:n_sub
+    fcon{i} = input(i).con1;
+end
 
 % SETUP BATCH JOB STRUCTURE
 second_level.matlabbatch{1}.spm.stats.factorial_design.dir = {stats_dir};
-second_level.matlabbatch{1}.spm.stats.factorial_design.des.t1.scans = '<UNDEFINED>';
+second_level.matlabbatch{1}.spm.stats.factorial_design.des.t1.scans = fcon';
 second_level.matlabbatch{1}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
 second_level.matlabbatch{1}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
 second_level.matlabbatch{1}.spm.stats.factorial_design.masking.tm.tm_none = 1;
@@ -21,5 +24,3 @@ second_level.matlabbatch{1}.spm.stats.factorial_design.globalm.glonorm = 1;
 
 % RUN BATCH JOB
 spm_jobman('run',second_level.matlabbatch);
-
-
