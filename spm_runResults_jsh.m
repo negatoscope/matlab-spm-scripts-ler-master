e0b0be7afe9742a6177a93cbd5ddac74d85e4b61
Luis Eudave <38909206+negatoscope@users.noreply.github.com>
2021-03-17 15:41:05 +0100

    Update spm_runResults_jsh.m

--- spm_runResults_jsh.m
+++ spm_runResults_jsh.m
@@ -1,4 +1,4 @@
-function spm_runResults_jsh(stats_dir)
+function spm_runResults_jsh(stats_dir, multiple, thresh)
 
 spm('defaults','fmri');
 spm_jobman('initcfg');
@@ -9,8 +9,8 @@ results.matlabbatch{1}.spm.stats.results.spmmat = {[stats_dir filesep 'SPM.mat']
 % conspec
 results.matlabbatch{1}.spm.stats.results.conspec.titlestr = '';
 results.matlabbatch{1}.spm.stats.results.conspec.contrasts = 1;
-results.matlabbatch{1}.spm.stats.results.conspec.threshdesc = 'FWE';
-results.matlabbatch{1}.spm.stats.results.conspec.thresh = 0.0500;
+results.matlabbatch{1}.spm.stats.results.conspec.threshdesc = multiple;
+results.matlabbatch{1}.spm.stats.results.conspec.thresh = thresh;
 results.matlabbatch{1}.spm.stats.results.conspec.extent = 0;
 results.matlabbatch{1}.spm.stats.results.conspec.conjunction = 1;
 results.matlabbatch{1}.spm.stats.results.conspec.mask.none = 1;
